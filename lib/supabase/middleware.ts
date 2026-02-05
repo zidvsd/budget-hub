f46aa5014783a8d61a4faf8ec3eb3e1a8fcfa43d
Zid <rashidvisda@gmail.com>
2026-02-05 16:30:40 +0800

    refactor middleware

---
+++ lib/supabase/middleware.ts
@@ -0,0 +1,37 @@
+import { createServerClient, type CookieOptions } from "@supabase/ssr";
+import { NextResponse, type NextRequest } from "next/server";
+
+export async function updateSession(request: NextRequest) {
+  let response = NextResponse.next({
+    request: { headers: request.headers },
+  });
+  const supabase = createServerClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL!,
+    process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!,
+    {
+      cookies: {
+        getAll() {
+          return request.cookies.getAll();
+        },
+        setAll(cookiesToSet) {
+          cookiesToSet.forEach(({ name, value, options }) =>
+            request.cookies.set(name, value),
+          );
+          response = NextResponse.next({ request });
+          cookiesToSet.forEach(({ name, value, options }) =>
+            response.cookies.set(name, value, options),
+          );
+        },
+      },
+    },
+  );
+  const {
+    data: { user },
+  } = await supabase.auth.getUser();
+
+  return {
+    supabase,
+    response,
+    user,
+  };
+}
--- proxy.ts
+++ proxy.ts
@@ -1,55 +1,27 @@
 // middleware.ts
 import { NextResponse, type NextRequest } from "next/server";
 import { createClient } from "./lib/supabase/server";
+import { updateSession } from "./lib/supabase/middleware";
 export async function proxy(request: NextRequest) {
-  let response = NextResponse.next({ request });
-  const supabase = await createClient();
-
-  const {
-    data: { user },
-  } = await supabase.auth.getUser();
-  const role = request.cookies.get("role")?.value;
+  const { user, response } = await updateSession(request);
   const url = request.nextUrl.pathname;
 
-  // 2. Protect CLient API Routes (Frontend)
-  if (url.startsWith("/api/client")) {
-    if (!user || role !== "user") {
-      return NextResponse.json(
-        { error: "Unauthorized: User access required" },
-        { status: 401 }
-      );
-    }
-  }
+  const isUserPage = ["/account", "/cart", "/orders", "/notifications"];
+  const isAdminPage = url.startsWith("/admin");
 
-  // 3. Protect API Routes ( Backend)
-  if (url.startsWith("/api/admin") && (!user || role !== "admin")) {
-    return NextResponse.json(
-      { error: "Unauthorized: Admin access required" },
-      { status: 401 }
-    );
-  }
-
-  // 4. Protect Admin Pages
-  if (url.startsWith("/admin") && (!user || role !== "admin")) {
+  if ((isUserPage || isAdminPage) && !user) {
     return NextResponse.redirect(new URL("/login", request.url));
   }
 
-  // 5. Protected User Routes (/account, /cart, /orders, /notifications)
-  const clientProtected = ["/account", "/cart", "/orders", "/notifications"];
-  const isClientPath = clientProtected.some((path) => url.startsWith(path));
+  const role = user?.app_metadata?.role || "user";
 
-  if (isClientPath) {
-    // If not logged in at all, or if they are an admin trying to access user-only pages
-    if (!user || role !== "user") {
-      return NextResponse.redirect(new URL("/login", request.url));
-    }
+  if (isAdminPage && role !== "admin") {
+    return NextResponse.redirect(new URL("/", request.url));
   }
 
-  // If an admin lands on the home page, send them to their dashboard
   if (url === "/" && role === "admin") {
     return NextResponse.redirect(new URL("/admin/dashboard", request.url));
   }
-
   return response;
 }
 
